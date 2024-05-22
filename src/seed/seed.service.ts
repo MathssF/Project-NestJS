d6621dd7ef2b0794b94716ccd424cbb7c9c50439
Matheus Flores <Maths.Aki@gmail.com>
2024-05-22 14:14:27 -0300

    seeds

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,6 +3,7 @@ import { GenreRepository } from 'src/movie/entities/genres.repository';
 import { MovieGenreRepository } from 'src/movie/entities/movie-genre.repository';
 import { MovieRepository } from 'src/movie/entities/movies.repository';
 import { MovieGenre } from 'src/movie/entities/movie-genre.entity';
+import { Rating } from 'src/user/entities/rating.entity';
 import { RatingRepository } from 'src/user/entities/rating.repository';
 import { UserRepository } from 'src/user/entities/user.repository';
 
@@ -19,6 +20,9 @@ export class SeedService {
   async seed() {
     await this.SeedUser();
     await this.SeedGenre();
+    await this.SeedMovie();
+    await this.SeedRelations();
+    await this.SeedRating();
   }
 
   private async SeedUser() {
@@ -259,12 +263,115 @@ export class SeedService {
         }
       });
       if (!exists) {
-        // await this.movieGenreRepository.save(relation as MovieInt);
         const movieGenre = new MovieGenre();
         movieGenre.movieId = relation.movie_id;
         movieGenre.genreId = relation.genre_id;
         await this.movieGenreRepository.save(movieGenre);
       }
     }
+  }
+
+  private async SeedRating() {
+    const votes = [
+        { userId: 4, movieId: 823464, rating: 5 },
+        { userId: 4, movieId: 653346, rating: 4 },
+        { userId: 4, movieId: 940721, rating: 3 },
+        { userId: 4, movieId: 1219685, rating: 5 },
+        { userId: 4, movieId: 996154, rating: 4 },
+        { userId: 4, movieId: 1093995, rating: 3 },
+        { userId: 4, movieId: 693134, rating: 4 },
+        { userId: 4, movieId: 1011985, rating: 5 },
+        { userId: 4, movieId: 967847, rating: 3 },
+        { userId: 4, movieId: 1094844, rating: 4 },
+        { userId: 4, movieId: 1111873, rating: 5 },
+        { userId: 4, movieId: 1096197, rating: 4 },
+        { userId: 4, movieId: 843527, rating: 5 },
+        { userId: 4, movieId: 748783, rating: 4 },
+        { userId: 4, movieId: 920342, rating: 3 },
+        { userId: 4, movieId: 119450, rating: 4 },
+        { userId: 4, movieId: 1064178, rating: 5 },
+  
+        { userId: 5, movieId: 823464, rating: 2 },
+        { userId: 5, movieId: 653346, rating: 1 },
+        { userId: 5, movieId: 940721, rating: 3 },
+        { userId: 5, movieId: 1219685, rating: 4 },
+        { userId: 5, movieId: 996154, rating: 3 },
+        { userId: 5, movieId: 1093995, rating: 5 },
+        { userId: 5, movieId: 693134, rating: 4 },
+        { userId: 5, movieId: 1011985, rating: 5 },
+        { userId: 5, movieId: 967847, rating: 3 },
+        { userId: 5, movieId: 1094844, rating: 4 },
+        { userId: 5, movieId: 1111873, rating: 5 },
+        { userId: 5, movieId: 1096197, rating: 4 },
+        { userId: 5, movieId: 843527, rating: 5 },
+        { userId: 5, movieId: 748783, rating: 4 },
+        { userId: 5, movieId: 920342, rating: 3 },
+        { userId: 5, movieId: 119450, rating: 4 },
+        { userId: 5, movieId: 1064178, rating: 5 },
+  
+        { userId: 6, movieId: 823464, rating: 3 },
+        { userId: 6, movieId: 653346, rating: 4 },
+        { userId: 6, movieId: 940721, rating: 5 },
+        { userId: 6, movieId: 1219685, rating: 4 },
+        { userId: 6, movieId: 996154, rating: 3 },
+  
+        { userId: 7, movieId: 823464, rating: 4 },
+        { userId: 7, movieId: 653346, rating: 5 },
+        { userId: 7, movieId: 940721, rating: 4 },
+        { userId: 7, movieId: 1219685, rating: 3 },
+        { userId: 7, movieId: 996154, rating: 5 },
+  
+        { userId: 8, movieId: 748783, rating: 4 },
+        { userId: 8, movieId: 920342, rating: 3 },
+        { userId: 8, movieId: 843527, rating: 5 },
+        { userId: 8, movieId: 1094844, rating: 4 },
+        { userId: 8, movieId: 1111873, rating: 3 },
+  
+        { userId: 9, movieId: 748783, rating: 5 },
+        { userId: 9, movieId: 920342, rating: 4 },
+        { userId: 9, movieId: 843527, rating: 3 },
+        { userId: 9, movieId: 1094844, rating: 5 },
+        { userId: 9, movieId: 1111873, rating: 4 },
+  
+        { userId: 10, movieId: 693134, rating: 4 },
+        { userId: 10, movieId: 1011985, rating: 5 },
+        { userId: 10, movieId: 967847, rating: 3 },
+        { userId: 10, movieId: 1096197, rating: 4 },
+        { userId: 10, movieId: 1064178, rating: 5 },
+  
+        { userId: 11, movieId: 1093995, rating: 4 },
+        { userId: 11, movieId: 1011985, rating: 3 },
+        { userId: 11, movieId: 967847, rating: 5 },
+        { userId: 11, movieId: 843527, rating: 4 },
+        { userId: 11, movieId: 1094844, rating: 3 },
+  
+        { userId: 12, movieId: 920342, rating: 4 },
+        { userId: 12, movieId: 119450, rating: 5 },
+        { userId: 12, movieId: 1064178, rating: 3 },
+        { userId: 12, movieId: 1111873, rating: 4 },
+        { userId: 12, movieId: 1096197, rating: 5 },
+    
+        { userId: 13, movieId: 843527, rating: 3 },
+        { userId: 13, movieId: 748783, rating: 4 },
+        { userId: 13, movieId: 920342, rating: 5 },
+        { userId: 13, movieId: 119450, rating: 4 },
+        { userId: 13, movieId: 1064178, rating: 3 },
+      ];
+      
+      for(const vote of votes){
+        const exists = await this.ratingRepository.findOne({
+          where: {
+            userId: vote.userId,
+            movieId: vote.movieId,
+          }
+        });
+
+        if (!exists) {
+          const rating = new Rating();
+          rating.userId = vote.userId;
+          rating.movieId = vote.movieId;
+          rating.rating = vote.rating;
+        }
+      }
   }
 }
