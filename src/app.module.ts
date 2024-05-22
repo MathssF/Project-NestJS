4a1d0f8ec53fb7d503236bf8c13ad1e84dbc1663
Matheus Flores <Maths.Aki@gmail.com>
2024-05-21 21:19:37 -0300

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { UserService } from './user/user.service';
+import { MovieService } from './movie/movie.service';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MovieModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, MovieService],
 })
 export class AppModule {}
