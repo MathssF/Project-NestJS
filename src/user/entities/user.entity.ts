e92537fc4d4cbf49998676284bd152585f1794d9
Matheus Flores <Maths.Aki@gmail.com>
2024-05-21 22:19:37 -0300

    user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ type: 'jsonb', default: {
+    vote: false,
+    edit: false,
+    add: false,
+    del: false,
+  } })
+  authority: {
+    vote: boolean,
+    edit: boolean,
+    add: boolean,
+    del: boolean,
+  };
+}
