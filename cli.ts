7744e946a9499f962a94f7190c7a2e09ef185b96
Matheus Flores <Maths.Aki@gmail.com>
2024-05-22 14:30:07 -0300

    wc

--- cli.ts
+++ cli.ts
@@ -7,17 +7,6 @@ async function seed() {
   const seedService = app.get(SeedService);
 
   try {
-    // console.log('Iniciando as seeds')
-    // await seedService.SeedUser();
-    // console.log('Seed User completed succesfully!')
-    // await seedService.SeedGenre();
-    // console.log('Seed Genre completed succesfully!')
-    // await seedService.SeedMovie();
-    // console.log('Seed Movie completed succesfully!')
-    // await seedService.SeedRelations();
-    // console.log('Seed Relations completed succesfully!')
-    // await seedService.SeedRating();
-    // console.log('Seed Votes completed successfully!');
     await seedService.seed();
   } catch (error) {
     console.error('Error occurred while seeding:', error);
