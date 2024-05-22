5573a9e71f84991ba1907c52225e3f1739a3a956
Matheus Flores <Maths.Aki@gmail.com>
2024-05-21 21:37:42 -0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
-import { UserService } from './user/user.service';
-import { MovieService } from './movie/movie.service';
 
 @Module({
   imports: [UserModule, MovieModule],
   controllers: [AppController],
-  providers: [AppService, UserService, MovieService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
-import { MovieService } from './movie.service';
+import { MovieController } from './movie.controller';
 
 @Module({
-  providers: [MovieService]
+  controllers: [MovieController],
+  providers: [MovieService],
 })
 export class MovieModule {}
