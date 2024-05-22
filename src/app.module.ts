bd6b7aa586be29d17fcbe89b986d4faf4106b0a5
Matheus Flores <Maths.Aki@gmail.com>
2024-05-21 21:48:50 -0300

    progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './user/user.module';
+// import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [UserModule, MovieModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // torna as configurações globais
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'MKSdatabase',
+      username: 'postgres',
+      password: '123456',
+
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
