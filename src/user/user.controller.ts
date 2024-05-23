735e41b9673ba4a0a705d17531d3f9bc65af9727
Matheus Flores <Maths.Aki@gmail.com>
2024-05-23 19:08:33 -0300

    auth -> user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,8 @@ import {
   Get,
   Param,
   NotFoundException,
+  Post,
+  Body,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
@@ -37,4 +39,16 @@ export class UserController {
   // async testDbConnection(): Promise<string> {
   //   return this.userService.testConnection();
   // }
+
+  @Post('register')
+  async register(@Body() registerDto: { username: string, email: string, password: string }) {
+    return this.userService.createUser(registerDto.username, registerDto.email, registerDto.password);
+  }
+  
+  // @Public()
+  @Post('login')
+  async login(@Body() loginDto: { username: string, password: string }) {
+    console.log('Passando pelo controller ');
+    return this.userService.login(loginDto.username, loginDto.password);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-// import { DataSource } from 'typeorm';
-// import { UserRepository } from './entities/user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    // private readonly dataSource: DataSource,
   ) {}
 
   async getUserByID(id: number): Promise<User | undefined> {
@@ -31,23 +29,39 @@ export class UserService {
     return user;
   }
 
-  // async testConnection(): Promise<string> {
-  //   try {
-  //     const queryRunner = this.dataSource.createQueryRunner();
-  //     await queryRunner.connect();
+  async createUser(username: string, email: string, password: string): Promise<User> {
+    const existingUser = await this.getUserByName(username);
+    if (existingUser) {
+      throw new ConflictException('Username already exists');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = this.userRepository.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(newUser);
+  }
 
-  //     const tableExists = await queryRunner.hasTable('user'); // Nome da tabela a verificar
+  async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.getUserByName(username);
+    if (user && await user.validatePassword(password)) {
+      return user;
+    }
+    throw new UnauthorizedException('Invalid credentials');
+  }
 
-  //     await queryRunner.release();
-
-  //     if (tableExists) {
-  //       return 'The table "user" exists!';
-  //     } else {
-  //       return 'The table "user" does not exist!';
-  //     }
-  //   } catch (error) {
-  //     console.error('Database connection failed:', error);
-  //     return 'Database connection failed!';
-  //   }
-  // }
+  async login(username: string, password: string): Promise<object> {
+    const wasUser = await this.validateUser(username, password);
+    const load = {user: username, sub: wasUser.id};
+    // const accessToken = await this.jwtService.signAsync(load);
+    // console.log('O erro pode estar no auth.service ');
+    // return { acess_token: accessToken };
+    if(wasUser) {
+      return {
+        id: wasUser.id,
+        User: wasUser.username
+      }
+    }
+  }
 }
