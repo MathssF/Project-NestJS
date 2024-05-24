0df3386ef11f02c3d6d322be099d4a1df03d51f1
Matheus Flores <Maths.Aki@gmail.com>
2024-05-24 14:19:09 -0300

    dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,43 @@
-export class CreateUserDto {}
+import {
+    IsString,
+    IsEmail,
+    MinLength,
+    MaxLength,
+    IsNotEmpty,
+    IsBoolean,
+    IsObject
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(25)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(30)
+  password: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsObject()
+  @IsNotEmpty()
+  authority: {
+    @IsBoolean()
+    vote: boolean;
+
+    @IsBoolean()
+    edit: boolean;
+
+    @IsBoolean()
+    add: boolean;
+
+    @IsBoolean()
+    del: boolean;
+  };
+}
