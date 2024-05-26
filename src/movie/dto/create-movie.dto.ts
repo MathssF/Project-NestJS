9e2ebe9be6d4e62d7604646999caab8890576b5e
Matheus Flores <Maths.Aki@gmail.com>
2024-05-26 11:01:32 -0300

    ajustesSwagger

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,20 +1,36 @@
 import { IsOptional, IsString, IsArray, IsNumber, IsDateString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 
 export class CreateMovieDto {
   @IsOptional()
+  @ApiProperty({ required: false })
   id?: number;
 
   @IsString()
+  @ApiProperty({
+    example: 'A Viagem de Chihiro',
+    description: `O nome do filme, usado para adicionar na lista, ou editar`,
+  })
   name: string;
 
   @IsString()
+  @ApiProperty({
+    example: 'Um Filme em estilo anime sobre uma menina emburrada que vive altas aventuras!',
+    description: `A descrição serve para explicar sobre o filme`,
+  })
   description: string;
 
   @IsDateString()
+  @ApiProperty({
+    example: '2 de Outubro de 2020',
+    description: `Serve para marcar a data que o filme foi criado`,
+  })
   release_date: string;
 
   @IsOptional()
   @IsArray()
   @IsNumber({}, { each: true })
+  @ApiProperty({ type: [Number], required: false })
   genres?: number[];
 }
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,30 +1,37 @@
 import { IsOptional, IsString, IsArray, IsNumber, IsDateString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EditMoviePost {
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false })
   name?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false })
   description?: string;
 
   @IsOptional()
   @IsDateString()
+  @ApiProperty({ required: false })
   release_date?: string;
 
   @IsOptional()
   @IsArray()
   @IsNumber({}, { each: true })
+  @ApiProperty({ required: false })
   listGenres?: number[];
 
   @IsOptional()
   @IsArray()
   @IsNumber({}, { each: true })
+  @ApiProperty({ required: false })
   addGenres?: number[];
 
   @IsOptional()
   @IsArray()
   @IsNumber({}, { each: true })
+  @ApiProperty({ required: false })
   delGenres?: number[];
 }
