58427d02a3ccb45ffedc51072a5c4e1faf1982c5
MathssF <102183898+MathssF@users.noreply.github.com>
2024-05-27 18:16:26 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,20 +7,12 @@ import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategys/local.strategy';
 import { JwtStrategy } from './strategys/jwt.strategy';
 import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
-// import { jwtConstants as jwt1 } from './constant/constant';
-// import { jwtConstants as jwt2 } from './constant/constant.example';
-
-
-// Obs: Aqui irá precisar ir no /src/auth/constant/constant.example.ts
-// E mudar de constant.example.ts para constant.ts
-// Ou então copie e crie um constant.ts na pasta (Fiz isto para Deploy)
 
 @Module({
   imports: [
     UserModule, PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET, //|| jwt1.secret || jwt2.secret,
-      // process.env.JWT_SECRET
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' },
     }),
   ],
